3628501f3b4163f53fd028b1ec40235022e1dd4a
vihjoulle <victordeghandi@gmail.com>
2024-11-14 19:52:07 -0300

    chore: start user-service-api

---
+++ user-service-api/src/main/java/br/com/victor/user_serviceapi/UserServiceApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.victor.user_serviceapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserServiceApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserServiceApiApplication.class, args);
+	}
+
+}
---
+++ user-service-api/src/test/java/br/com/victor/user_serviceapi/UserServiceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.victor.user_serviceapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
